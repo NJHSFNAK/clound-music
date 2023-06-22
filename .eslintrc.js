d1c1a9a4115b810d85e969496301fdeb6b0ba5ba
韩煦 <18232241431@163.com>
2023-06-22 14:39:41 +0800

    refactor: 代码上传

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,34 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: "latest", // 使用最新的 ECMAScript 版本
-    sourceType: "module" // 使用模块化的文件结构
-  },
   env: {
-    browser: true, // 启用浏览器环境
-    es2021: true, // 使用 ES2021 版本的特性
-    commonjs: true // 启用 CommonJS 模块规范
+    browser: true,
+    es2021: true,
+    // 代码同时运行在浏览器和 Node.js 中
+    node: true
   },
-  parser: "@typescript-eslint/parser", // 使用 '@typescript-eslint/parser' 作为解析器，用于解析 TypeScript 代码
+  parser: "@typescript-eslint/parser",
   extends: [
-    "eslint:recommended", // 使用 ESLint 推荐的基本规则
-    "plugin:react/recommended", // 使用 react 插件推荐的规则
-    "plugin:@typescript-eslint/recommended", // 使用 @typescript-eslint 插件推荐的规则
-    // 引入prettier
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    // 同时检测代码是否符合eslint和prettier的规范
     "plugin:prettier/recommended"
   ],
-  plugins: ["react"], // 启用 react 插件
-  rules: {
-    quotes: ["error"] // 强制使用单引号
-  }
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script"
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint", "react"],
+  // elint的一些规则
+  rules: {}
 };
